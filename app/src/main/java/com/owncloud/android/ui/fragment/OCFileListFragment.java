b7080e41843fc1ad46002ed617e30e591aec285f
tobiasKaminsky <tobias@kaminsky.me>
2022-10-06 14:56:41 +0200

    allow to create encrypted folder directly via bottom sheet dialog
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -511,6 +511,14 @@ public class OCFileListFragment extends ExtendedListFragment implements
     }
 
     @Override
+    public void createEncryptedFolder() {
+        if (checkEncryptionIsSetup(null)) {
+            CreateFolderDialogFragment.newInstance(mFile, true)
+                .show(getActivity().getSupportFragmentManager(), DIALOG_CREATE_FOLDER);
+        }
+    }
+
+    @Override
     public void uploadFromApp() {
         Intent action = new Intent(Intent.ACTION_GET_CONTENT);
         action = action.setType("*/*").addCategory(Intent.CATEGORY_OPENABLE);
@@ -1161,10 +1169,17 @@ public class OCFileListFragment extends ExtendedListFragment implements
             int position = data.getIntExtra(SetupEncryptionDialogFragment.ARG_POSITION, -1);
             OCFile file = mAdapter.getItem(position);
 
+            // TODO needed?
             if (file != null) {
                 mContainerActivity.getFileOperationsHelper().toggleEncryption(file, true);
                 mAdapter.setEncryptionAttributeForItemID(file.getRemoteId(), true);
+           }
+
+            if (file == null) {
+                return;
             }
+            
+            encryptFolder(file.getLocalId(), file.getRemoteId(), file.getRemotePath(), true);
 
             // update state and view of this fragment
             searchFragment = false;
@@ -1781,6 +1796,12 @@ public class OCFileListFragment extends ExtendedListFragment implements
 
     @Subscribe(threadMode = ThreadMode.BACKGROUND)
     public void onMessageEvent(EncryptionEvent event) {
+        if (checkEncryptionIsSetup(event.remoteId)) {
+            encryptFolder(event.localId, event.remoteId, event.remotePath, event.shouldBeEncrypted);
+        }
+    }
+
+    private boolean checkEncryptionIsSetup(@Nullable String remoteId) {
         final User user = accountManager.getUser();
 
         // check if keys are stored
@@ -1794,12 +1815,17 @@ public class OCFileListFragment extends ExtendedListFragment implements
             Log_OC.d(TAG, "no public key for " + user.getAccountName());
 
             int position = -1;
-            if (file != null) {
-                position = mAdapter.getItemPosition(file);
+            if (remoteId != null) {
+                OCFile file = storageManager.getFileByRemoteId(remoteId);
+                if (file != null) {
+                    position = mAdapter.getItemPosition(file);
+                }
             }
             SetupEncryptionDialogFragment dialog = SetupEncryptionDialogFragment.newInstance(user, position);
             dialog.setTargetFragment(this, SETUP_ENCRYPTION_REQUEST_CODE);
             dialog.show(getParentFragmentManager(), SETUP_ENCRYPTION_DIALOG_TAG);
+            
+            return false;
         } else {
             // TODO E2E: if encryption fails, to not set it as encrypted!
             encryptFolder(file,
